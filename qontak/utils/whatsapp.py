e52a84d8dc32f2855981ef4742fe19325ff4f297
Aslam <aslam.hafidz@meteor.id>
2023-03-20 12:27:11 +0700

    Add whatsapp utils

---
+++ qontak/utils/whatsapp.py
@@ -0,0 +1,6 @@
+from phonenumbers import parse, format_number, PhoneNumberFormat
+
+
+def whatsapp_phone_number(phone_number, region=None):
+    return format_number(
+        parse(phone_number, region), PhoneNumberFormat.E164)
